dbd01345708e271eddcef2852e8156f08ef5703e
hnnhpark <hnnhhy.park@gmail.com>
2021-09-13 02:15:10 +0900

    add _site to .gitignore

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "[Swift] Swift 기본문법 정리",
-        "excerpt":"hello world  ","categories": ["Swift"],
-        "tags": ["swift"],
-        "url": "/swift/swift-basic/",
-        "teaser": null
-      }]
